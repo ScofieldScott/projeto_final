02da226bfdc4e6662e3db1a53735c5004151da92
ScofieldScott <anascottbeatriz@gmail.com>
2023-06-23 23:21:06 -0300

    it's working, for now

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-//const replaceTamplate = require('');
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const replaceTamplate = require('./module/replaceTemplate');
+const data = fs.readFileSync(`${__dirname}/dev-data/pessoa.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
     const {query, pathname} = url.parse(req.url, true);
 
-    if(pathname === '/'){
-        res.writeHead(2--, {'Content-type': 'text/html'})
+    if(pathname === '/' || pathname === '/homePage'){
+        //res.writeHead(2--, {'Content-type': 'text/html'});
+        res.end('This the home page!');
     }
 });
 
-Server.listen(8000, '127.0.0.1', () => {
+server.listen(8000, '127.0.0.1', () => {
     console.log('Listenig to request on port 8000');
 });
